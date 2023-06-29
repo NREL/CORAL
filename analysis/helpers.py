f15af721628465f99be193d15bb19d1433e2ae6b
Shields <mshields@nrel.gov>
2023-06-29 08:59:44 -0600

    Revert wc-pipeline back to 16 GW in Humboldt

--- analysis/helpers.py
+++ analysis/helpers.py
@@ -10,50 +10,50 @@ allocations = {
     'Baseline-Low': {
         "ahts_vessel": ('example_ahts_vessel', 1), # =3 AHTS (1 projects at a time, 3 groups per project, 1 AHTS per group)
         "towing_vessel": ('example_towing_vessel', 1), # =6 tugs (1 projects at a time, 3 groups per project, 2 tugs per group)
-        "mooring_install_vessel": ('example_support_vessel', 1), # =1 AHTS (1 projects at a time, 1 per project)
-        "array_cable_install_vessel": ('example_array_cable_lay_vessel', 1), # =1 CLVs (1 projects at a time, 1 CLV per project)
-        "export_cable_install_vessel": ("example_export_cable_lay_vessel", 1), # =1 CLVs (1 projects at a time, 1 CLV per project)
+        # "mooring_install_vessel": ('example_support_vessel', 1), # =1 AHTS (1 projects at a time, 1 per project)
+        # "array_cable_install_vessel": ('example_array_cable_lay_vessel', 1), # =1 CLVs (1 projects at a time, 1 CLV per project)
+        # "export_cable_install_vessel": ("example_export_cable_lay_vessel", 1), # =1 CLVs (1 projects at a time, 1 CLV per project)
         "port": [('Humboldt', 1)]
             },
     ## Saturate # of vessels to iterate on port constraints and wait for data on actual numbers
     'Baseline-Mid (SC)': {
         "ahts_vessel": ('example_ahts_vessel', 3), # =9 AHTS
         "towing_vessel": ('example_towing_vessel', 3), # =18 tugs
-        "mooring_install_vessel": ('example_support_vessel', 3), # =3 AHTS
-        "array_cable_install_vessel": ('example_array_cable_lay_vessel', 3), # =3 CLV (array)
-        "export_cable_install_vessel": ("example_export_cable_lay_vessel", 3), # =3 CLV (export)
+        # "mooring_install_vessel": ('example_support_vessel', 3), # =3 AHTS
+        # "array_cable_install_vessel": ('example_array_cable_lay_vessel', 3), # =3 CLV (array)
+        # "export_cable_install_vessel": ("example_export_cable_lay_vessel", 3), # =3 CLV (export)
         "port": [('Long Beach', 0), ('Humboldt', 1)]
             },
     'Baseline-Mid (CC)': {
         "ahts_vessel": ('example_ahts_vessel', 2), # =6 AHTS
         "towing_vessel": ('example_towing_vessel', 2), # =12 tugs
-        "mooring_install_vessel": ('example_support_vessel', 2), # =2 AHTS
-        "array_cable_install_vessel": ('example_array_cable_lay_vessel', 2), # =2 CLV (array)
-        "export_cable_install_vessel": ("example_export_cable_lay_vessel",2), # =2 CLV (export)
+        # "mooring_install_vessel": ('example_support_vessel', 2), # =2 AHTS
+        # "array_cable_install_vessel": ('example_array_cable_lay_vessel', 2), # =2 CLV (array)
+        # "export_cable_install_vessel": ("example_export_cable_lay_vessel",2), # =2 CLV (export)
         "port": [('Port of San Luis', 0), ('Humboldt', 1)]
             },
     'Moderate-Low': {
         "ahts_vessel": ('example_ahts_vessel', 3), # =9 AHTS
         "towing_vessel": ('example_towing_vessel', 3), # =18 tugs
-        "mooring_install_vessel": ('example_support_vessel', 3), # =3 AHTS
-        "array_cable_install_vessel": ('example_array_cable_lay_vessel', 3), # =3 CLV (array)
-        "export_cable_install_vessel": ("example_export_cable_lay_vessel",3), # =3 CLV (export)
+        # "mooring_install_vessel": ('example_support_vessel', 3), # =3 AHTS
+        # "array_cable_install_vessel": ('example_array_cable_lay_vessel', 3), # =3 CLV (array)
+        # "export_cable_install_vessel": ("example_export_cable_lay_vessel",3), # =3 CLV (export)
         "port": [('Humboldt', 1), ('Coos Bay', 0)]
             },
     'Moderate-Mid (SC)': {
         "ahts_vessel": ('example_ahts_vessel', 4), # =12 AHTS
         "towing_vessel": ('example_towing_vessel', 4), # =24 tugs
-        "mooring_install_vessel": ('example_support_vessel', 4), # =4 AHTS
-        "array_cable_install_vessel": ('example_array_cable_lay_vessel', 4), # =4 CLV (array)
-        "export_cable_install_vessel": ("example_export_cable_lay_vessel",4), # =4 CLV (export)
+        # "mooring_install_vessel": ('example_support_vessel', 4), # =4 AHTS
+        # "array_cable_install_vessel": ('example_array_cable_lay_vessel', 4), # =4 CLV (array)
+        # "export_cable_install_vessel": ("example_export_cable_lay_vessel",4), # =4 CLV (export)
         "port": [('Long Beach', 0), ('Humboldt', 1), ('Coos Bay', 0)]
             },
     'Expanded-High': {
         "ahts_vessel": ('example_ahts_vessel', 8), # =24 AHTS
         "towing_vessel": ('example_towing_vessel', 8), # =48 tugs
-        "mooring_install_vessel": ('example_support_vessel', 8), # =8 AHTS
-        "array_cable_install_vessel": ('example_array_cable_lay_vessel', 8), # =8 CLV (array)
-        "export_cable_install_vessel": ("example_export_cable_lay_vessel", 8), # =8 CLV (export)
+        # "mooring_install_vessel": ('example_support_vessel', 8), # =8 AHTS
+        # "array_cable_install_vessel": ('example_array_cable_lay_vessel', 8), # =8 CLV (array)
+        # "export_cable_install_vessel": ("example_export_cable_lay_vessel", 8), # =8 CLV (export)
         "port": [('Long Beach', 0), ('Humboldt', 1), ('Coos Bay', 0), ('Grays Harbor', 0), ('Port of San Luis', 0)]
             }
 }
--- analysis/main.py
+++ analysis/main.py
@@ -15,8 +15,8 @@ from plot_routines import plot_gantt, plot_throughput, plot_gantt_nt, assign_col
 
 # Configure scenarios and keep_inputs
 projects = "library/pipeline/wc-pipeline.xlsx"
-scenarios = ['Baseline-Low', 'Baseline-Mid (SC)', 'Baseline-Mid (CC)', 'Moderate-Low', 'Moderate-Mid (SC)', 'Expanded-High']
-#scenarios = ['Expanded-High']
+# scenarios = ['Baseline-Low', 'Baseline-Mid (SC)', 'Baseline-Mid (CC)', 'Moderate-Low', 'Moderate-Mid (SC)', 'Expanded-High']
+scenarios = ['Baseline-Mid (SC)']
 base = "base.yaml"
 library_path = "library"
 weather_path = "library/weather/humboldt_weather_2010_2018.csv"
